8b0b8d8eb6b59c08bbc49e69a4459c44189c2b2d
amendb <amendozab98@gmail.com>
2024-12-19 22:15:02 +0100

    Back to normality

--- Evaluation/Metrics_figs.py
+++ Evaluation/Metrics_figs.py
@@ -8,15 +8,15 @@ import os
 
 # Read the data
 folders = [
-    'Evaluation/Results/delta', 
+    'Evaluation/Results/alpha', 
     # 'Evaluation/Results/acoruna_port',     
     # 'Evaluation/Results/comb_port',
     ]
 for folder in folders:
     paths = [
-        f'{folder}/DRLIndependent.4.negativedistance_1.0_10.0_2.0_1.0/metrics.csv',
-        f'{folder}/DRLIndependent.4.negativedistance_1_50_2_1/metrics.csv',
-        f'{folder}/DRLIndependent.4.negativedistance_1.0_100.0_2.0_1.0/metrics.csv',
+        f'{folder}/DRLIndependent.4.negativedistance_1.0_50.0_2.0_0.1/metrics.csv',
+        f'{folder}/DRLIndependent.4.negativedistance_1_50_2_1_baseline/metrics.csv',
+        f'{folder}/DRLIndependent.4.negativedistance_1.0_50.0_2.0_10.0/metrics.csv',
         # f'{folder}/DRLIndependentgreedy.4.negativedistance_1_50_2_0/metrics.csv',
         # f'{folder}/DRLIndependent.4.negativedistance_1_50_2_0/metrics.csv',        
         # f'{folder}/Greedy.4.negativedistance_1_50_2_0/metrics.csv',
